a224817a50ceef412d553fa9e0cf226b59ed4edb
Vincent Escueta <vincent.escueta22@berkeley.edu>
2018-12-06 13:56:18 -0800

    here

--- js/faded.js
+++ js/faded.js
@@ -1,3 +1,13 @@
 $(function() {
     $('body').removeClass('fade-out');
 });
+
+/*! Fades out the whole page when clicking links */
+$('a').click(function(e) {
+e.preventDefault();
+newLocation = this.href;
+$('body').fadeOut('slow', newpage);
+});
+function newpage() {
+window.location = newLocation;
+}
